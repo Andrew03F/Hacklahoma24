679f74f49dd4a4fb40c749c517eb443a4affb3f3
Andrew Finely <andrewfinely@Andrews-MacBook-Air-6.local>
2024-02-10 12:14:09 -0600

    remove old stuff

--- sketch.js
+++ sketch.js
@@ -1,19 +1,7 @@
 
 let w = 500;
 let h = 500;
-let cells = 33;
-let spacer = (w - 1) / cells;
-let m;
-let s;
-let v;
-let stack = [];
-let ls;
-let safe = false;
-let ld = .53;
-let button;
-let reset = false;
-let speed;
-let padtop = 90;
+
 function setup() {
   var canvas = createCanvas(w, h);
   canvas.parent('sketch-holder');
